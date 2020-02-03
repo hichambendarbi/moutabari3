a1805f709d1ee9086662755636d00c464d237622
khmmamed <khmammed@gmail.com>
2020-02-03 22:02:48 +0100

    test

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  app.get("*", (requestAnimationFrame, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
