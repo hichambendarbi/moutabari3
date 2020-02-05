5ae18b16ff9a1d91f7eddf509ba7a36ba5a47ec5
Hicham Bendarbi <49118568+hichambendarbi@users.noreply.github.com>
2020-02-05 18:46:51 +0100

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://hicham123:hicham123@devconnector-gv4xg.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://yournameatlas:yourpassword@devconnector-gv4xg.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret"
   };
