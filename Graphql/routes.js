b94e9904af8e73805556e445a6b9f957614a7f23
Mkhmalech <m.khmalech@gmail.com>
2020-02-08 14:13:23 +0100

    correct routes

--- Graphql/routes.js
+++ Graphql/routes.js
@@ -1,11 +1,11 @@
-var routes =($) => {
+module.exports =($) => {
     
     $.get("/", ( req , res , next ) =>{  res.send('<bold> Hello World </b>') });
 
     $.all('/users', (req, res)=>{
 
         res.send("helloooo")
-        
+
     });
 
     return $;
--- Graphql/server.js
+++ Graphql/server.js
@@ -1,6 +1,7 @@
 var express = require('./node_modules/express')
 var app = express();
 var bodyParser = require('./node_modules/body-parser');
+var routes = require('./routes');
 
 app.use((req, res, next) => {
     next();
@@ -10,4 +11,6 @@ app.use(bodyParser.json());
 app.use('/', routes(express.Router()));
 
 
-app.listen(8080);
+app.listen(8000, () => {
+    console.log(`application listening on port : ${8000}`)
+})
